85e78cb1a7fbb33b1ef4d4a8b4220779f92e1a20
kkirino <kosuke.kirino@gmail.com>
2022-05-10 20:08:42 +0900

    change ui

--- src/script.js
+++ src/script.js
@@ -11,6 +11,7 @@ function openSidebar() {
 }
 
 const ss = SpreadsheetApp.getActiveSpreadsheet();
+const dayOfWeekJa = ["日", "月", "火", "水", "木", "金", "土"];
 
 function getSettings() {
   const ws = ss.getSheetByName("settings");
@@ -32,12 +33,28 @@ function getSettings() {
   settings.calendarIdOutside =
     values[titles.indexOf("ネーベン・外勤のカレンダーID")];
   settings.manualPageUrl = values[titles.indexOf("マニュアルページのURL")];
-  holidayStringsArray = values[titles.indexOf("追加の祝日")].split(",");
-  settings.holidays = holidayStringsArray.map(function (value) {
+  const holidayStrings = values[titles.indexOf("追加の祝日")].split(",");
+  settings.holidays = holidayStrings.map(function (value) {
     return {
       month: value.split("/")[0] - 1,
       date: value.split("/")[1],
     };
+  });
+  const outpatientDayStrings =
+    values[titles.indexOf("大学当番外来係の曜日")].split(",");
+  const firstAidCenterDayStrings =
+    values[titles.indexOf("大学当番急患センターの曜日")].split(",");
+  settings.outpatientDays = [];
+  settings.firstAidCenterDays = [];
+  outpatientDayStrings.forEach(function (value) {
+    if (dayOfWeekJa.indexOf(value) > -1) {
+      settings.outpatientDays.push(dayOfWeekJa.indexOf(value));
+    }
+  });
+  firstAidCenterDayStrings.forEach(function (value) {
+    if (dayOfWeekJa.indexOf(value) > -1) {
+      settings.firstAidCenterDays.push(dayOfWeekJa.indexOf(value));
+    }
   });
   return settings;
 }
@@ -48,31 +65,36 @@ function getDefinition() {
     .getRange(2, 1, ws.getLastRow() - 1, ws.getLastColumn())
     .getValues();
   const definition = [];
-  const dayOfWeekJa = ["日", "月", "火", "水", "木", "金", "土"];
   sheetInfo.forEach(function (row) {
     for (let i = 0; i < 1 + 4 * Number(row[2] === 99); i++) {
       for (let j = 0; j < 1 + Number(row[5] === "通常・祝日共通"); j++) {
-        for (let k = 0; k < 1 + Number(row[9] === "通常・祝日共通"); k++) {
-          definition.push({
-            hospital: row[0],
-            work: row[1],
-            weekInMonthStart:
-              (i + 1) * Number(row[2] === 99) + row[2] * Number(row[2] !== 99),
-            dayOfWeekStart: dayOfWeekJa.indexOf(row[3]),
-            isSaturdayCount: Number(row[4] === "はい"),
-            isHolidayStart:
-              j * Number(row[5] === "通常・祝日共通") +
-              Number(row[5] === "祝日"),
-            hoursStart: row[6],
-            minutesStart: row[7],
-            isNextDayEnd: Number(row[8] === "翌日"),
-            isHolidayEnd:
-              k * Number(row[9] === "通常・祝日共通") +
-              Number(row[9] === "祝日"),
-            hoursEnd: row[10],
-            minutesEnd: row[11],
-            doctorInDefault: row[12],
-          });
+        for (let k = 0; k < 1 + Number(row[6] === "通常・祝日共通"); k++) {
+          for (let l = 0; l < 1 + Number(row[10] === "通常・祝日共通"); l++) {
+            definition.push({
+              hospital: row[0],
+              work: row[1],
+              weekInMonthStart:
+                (i + 1) * Number(row[2] === 99) +
+                row[2] * Number(row[2] !== 99),
+              dayOfWeekStart: dayOfWeekJa.indexOf(row[3]),
+              isSaturdayCount: Number(row[4] === "はい"),
+              isHolidayPrevious:
+                j * Number(row[5] === "通常・祝日共通") +
+                Number(row[5] === "祝日"),
+              isHolidayStart:
+                k * Number(row[6] === "通常・祝日共通") +
+                Number(row[6] === "祝日"),
+              hoursStart: row[7],
+              minutesStart: row[8],
+              isNextDayEnd: Number(row[9] === "翌日"),
+              isHolidayEnd:
+                l * Number(row[10] === "通常・祝日共通") +
+                Number(row[10] === "祝日"),
+              hoursEnd: row[11],
+              minutesEnd: row[12],
+              doctorInDefault: row[13],
+            });
+          }
         }
       }
     }
@@ -80,14 +102,15 @@ function getDefinition() {
   return definition;
 }
 
+function reduceSum(previousValue, currentValue) {
+  return previousValue + currentValue;
+}
+
 function isHoliday(date) {
   const holidaysInCalendar = CalendarApp.getCalendarById(
     "ja.japanese#holiday@group.v.calendar.google.com"
   );
   const holidaysInSettings = getSettings().holidays;
-  function reduceSum(previousValue, currentValue) {
-    return previousValue + currentValue;
-  }
   return Number(
     holidaysInCalendar.getEventsForDay(date).length > 0 ||
       holidaysInSettings
@@ -106,9 +129,9 @@ function getDateInfo(userInfo) {
   const duration =
     (dateTo.valueOf() - dateFrom.valueOf()) / (1000 * 60 * 60 * 24) + 1;
   const dateInfo = Array.from(new Array(duration)).map(function (v, i) {
-    let dateStart = new Date(dateFrom.valueOf() + 1000 * 60 * 60 * 24 * i);
-    let datePrevious = new Date(dateStart.valueOf() - 1000 * 60 * 60 * 24);
-    let dateNext = new Date(dateStart.valueOf() + 1000 * 60 * 60 * 24);
+    const dateStart = new Date(dateFrom.valueOf() + 1000 * 60 * 60 * 24 * i);
+    const datePrevious = new Date(dateStart.valueOf() - 1000 * 60 * 60 * 24);
+    const dateNext = new Date(dateStart.valueOf() + 1000 * 60 * 60 * 24);
     return {
       dateStart: dateStart,
       weekInMonthStart: [
@@ -116,12 +139,13 @@ function getDateInfo(userInfo) {
         Math.floor((datePrevious.getDate() + 6) / 7),
       ],
       dayOfWeekStart: dateStart.getDay(),
+      isHolidayPrevious: isHoliday(datePrevious),
       isHolidayStart: isHoliday(dateStart),
       dateEnd: [dateStart, dateNext],
       isHolidayEnd: [isHoliday(dateStart), isHoliday(dateNext)],
     };
   });
-  return dateInfo
+  return dateInfo;
 }
 
 function createSchedules(userInfo) {
@@ -131,60 +155,107 @@ function createSchedules(userInfo) {
     try {
       const dateInfo = getDateInfo(userInfo);
       const definition = getDefinition();
-      const inside = ss.insertSheet();
-      inside.setName("【作業中】大学当番");
-      const outside = ss.insertSheet();
-      outside.setName("【作業中】外勤");
-      dateInfo.forEach(function (date) {
-        if (
-          date.dayOfWeekStart === 0 ||
-          date.dayOfWeekStart === 6 ||
-          date.isHolidayStart === 1
-        ) {
-          ["当直", "Super", "オンコール"].forEach(function (value) {
-            inside.appendRow([date.dateStart.toLocaleDateString(), value]);
-          });
-        } else if (
-          date.dayOfWeekStart === 1 ||
-          date.dayOfWeekStart === 3 ||
-          date.dayOfWeekStart === 5
-        ) {
-          ["外来係", "当直", "Super", "オンコール", "急患センター"].forEach(
-            function (value) {
-              inside.appendRow([date.dateStart.toLocaleDateString(), value]);
-            }
+      const settings = getSettings();
+      const tmpForInside = ss.getSheetByName("template_大学当番");
+      const tmpForOutside = ss.getSheetByName("template_外勤");
+      const sheetNames = ss.getSheets().map(function (sheet) {
+        return sheet.getName();
+      });
+      const sheetNamesWithOldDate = sheetNames
+        .filter(function (string) {
+          const regex = new RegExp(
+            "^20[0-9]{2}/[0-9]{1,}/[0-9]{1,} [0-9]{1,}:[0-9]{1,}:[0-9]{1,}$"
           );
-        } else {
-          ["当直", "Super", "オンコール", "急患センター"].forEach(function (
-            value
-          ) {
-            inside.appendRow([date.dateStart.toLocaleDateString(), value]);
+          return string.search(regex) > -1;
+        })
+        .filter(function (string) {
+          return (
+            (new Date().valueOf() - new Date(string).valueOf()) /
+              (1000 * 60 * 60 * 24) >
+            90
+          );
+        });
+      sheetNamesWithOldDate.forEach(function (name) {
+        const sheetForDelete = ss.getSheetByName(name);
+        ss.deleteSheet(sheetForDelete);
+      });
+      const sheetNameForInside = "【作業中】大学当番";
+      const sheetNameForOutside = "【作業中】外勤";
+      if (
+        sheetNames.indexOf(sheetNameForInside) > -1 ||
+        sheetNames.indexOf(sheetNameForOutside) > -1
+      ) {
+        return "作業中シートが既に存在します。新たなシートは作成しませんでした。";
+      } else {
+        const inside = tmpForInside.copyTo(ss);
+        inside.setName(sheetNameForInside);
+        inside
+          .getRange(2, 1, inside.getLastRow() - 1, inside.getLastColumn())
+          .clearContent();
+        const outside = tmpForOutside.copyTo(ss);
+        outside.setName(sheetNameForOutside);
+        outside
+          .getRange(2, 1, outside.getLastRow() - 1, outside.getLastColumn())
+          .clearContent();
+        const insideInfo = [];
+        const outsideInfo = [];
+        dateInfo.forEach(function (date) {
+          const needForOutpatient =
+            settings.outpatientDays
+              .map(function (i) {
+                return i === date.dayOfWeekStart;
+              })
+              .reduce(reduceSum) > 0;
+          const needForFirstAidCenter =
+            settings.firstAidCenterDays
+              .map(function (i) {
+                return i === date.dayOfWeekStart;
+              })
+              .reduce(reduceSum) > 0;
+          ["当直", "Super", "オンコール"].forEach(function (value) {
+            insideInfo.push([date.dateStart.toLocaleDateString(), value]);
           });
-        }
-        definition.forEach(function (def) {
-          if (
-            def.weekInMonthStart ===
-              date.weekInMonthStart[def.isSaturdayCount] &&
-            def.dayOfWeekStart === date.dayOfWeekStart &&
-            def.isHolidayStart === date.isHolidayStart &&
-            def.isHolidayEnd === date.isHolidayEnd[def.isNextDayEnd]
-          ) {
-            outside.appendRow([
-              def.hospital,
-              def.work,
-              def.doctorInDefault,
+          if (needForFirstAidCenter && !isHoliday(date.dateStart)) {
+            insideInfo.push([
               date.dateStart.toLocaleDateString(),
-              def.hoursStart,
-              def.minutesStart,
-              date.dateEnd[def.isNextDayEnd].toLocaleDateString(),
-              def.hoursEnd,
-              def.minutesEnd,
+              "急患センター",
             ]);
           }
+          if (needForOutpatient && !isHoliday(date.dateStart)) {
+            // inside.appendRow([date.dateStart.toLocaleDateString(), "外来係"]);
+            insideInfo.push([date.dateStart.toLocaleDateString(), "外来係"]);
+          }
+          definition
+            .filter(function (def) {
+              return (
+                def.weekInMonthStart ===
+                  date.weekInMonthStart[def.isSaturdayCount] &&
+                def.dayOfWeekStart === date.dayOfWeekStart &&
+                def.isHolidayPrevious === date.isHolidayPrevious &&
+                def.isHolidayStart === date.isHolidayStart &&
+                def.isHolidayEnd === date.isHolidayEnd[def.isNextDayEnd]
+              );
+            })
+            .forEach(function (def) {
+              outsideInfo.push([
+                def.hospital,
+                def.work,
+                def.doctorInDefault,
+                date.dateStart.toLocaleDateString(),
+                def.hoursStart,
+                def.minutesStart,
+                date.dateEnd[def.isNextDayEnd].toLocaleDateString(),
+                def.hoursEnd,
+                def.minutesEnd,
+              ]);
+            });
         });
-      });
-      return "予定を作成しました";
-    } catch {
+        inside.getRange(2, 1, insideInfo.length, 2).setValues(insideInfo);
+        outside.getRange(2, 1, outsideInfo.length, 9).setValues(outsideInfo);
+        return "予定を作成しました";
+      }
+    } catch (e) {
+      Logger.log(e);
       return "予定を作成できませんでした";
     }
   }
@@ -192,52 +263,117 @@ function createSchedules(userInfo) {
 
 function writeCalendarEvents(isCheckedInside, isCheckedOutside) {
   if (!isCheckedInside && !isCheckedOutside) {
-    return "チェックボックスにチェックを入れてください";
+    return "どちらかの予定を選んでください";
   } else {
-    try {
-      if (isCheckedOutside) {
-        const calendarId = getSettings().calendarIdOutside;
-        const calendar = CalendarApp.getCalendarById(calendarId);
-        const ws = ss.getSheetByName("【作業中】外勤");
-        const sheetInfo = ws
-          .getRange(1, 1, ws.getLastRow(), ws.getLastColumn())
-          .getValues();
-        sheetInfo.forEach(function (row) {
-          let title = row[2] + " " + row[0] + row[1];
-          let startTime = new Date(row[3]);
-          startTime.setHours(row[4]);
-          startTime.setMinutes(row[5]);
-          let endTime = new Date(row[6]);
-          endTime.setHours(row[7]);
-          endTime.setMinutes(row[8]);
-          calendar.createEvent(title, startTime, endTime);
-        });
-        const today = new Date();
-        ws.setName(
-          today.toLocaleDateString() + " " + today.toLocaleTimeString()
-        );
-      }
-      if (isCheckedInside) {
+    if (isCheckedInside) {
+      try {
         const calendarId = getSettings().calendarIdInside;
         const calendar = CalendarApp.getCalendarById(calendarId);
         const ws = ss.getSheetByName("【作業中】大学当番");
         const sheetInfo = ws
-          .getRange(1, 1, ws.getLastRow(), ws.getLastColumn())
+          .getRange(2, 1, ws.getLastRow() - 1, ws.getLastColumn())
           .getValues();
-          Logger.log(sheetInfo);
-        sheetInfo.forEach(function (row) {
-          let title = "【" + row[1] + "】" + row[2];
-          let date = new Date(row[0]);
-          calendar.createAllDayEvent(title, date);
+        const schedules = [];
+        let error = 0;
+        sheetInfo.forEach(function (row, index) {
+          const title = "【" + row[1] + "】" + row[2];
+          const date = new Date(row[0]);
+          const blankExists =
+            row
+              .map(function (value) {
+                return value === "";
+              })
+              .reduce(reduceSum) > 0;
+          if (blankExists) {
+            ws.getRange(2 + index, 4).setValue(
+              "この行に空欄があります(あとで消してください)"
+            );
+            error += 1;
+          }
+          schedules.push({
+            title: title,
+            date: date,
+          });
         });
-        const today = new Date();
-        ws.setName(
-          today.toLocaleDateString() + " " + today.toLocaleTimeString()
-        );
+        if (error > 0) {
+          return "予定の記載に空欄があります";
+        } else {
+          schedules.forEach(function (schedule) {
+            calendar.createAllDayEvent(schedule.title, schedule.date);
+            Utilities.sleep(500);
+          });
+          const today = new Date();
+          ws.setName(
+            today.toLocaleDateString() + " " + today.toLocaleTimeString()
+          );
+          const protection = ws.protect();
+          protection.setWarningOnly(true);
+          return "予定をカレンダーに登録しました";
+        }
+      } catch (e) {
+        Logger.log(e);
+        return "予定をカレンダーに登録できませんでした";
       }
-      return "予定をカレンダーに登録しました";
-    } catch {
-      return "予定をカレンダーに登録できませんでした";
+    } else {
+      try {
+        const calendarId = getSettings().calendarIdOutside;
+        const calendar = CalendarApp.getCalendarById(calendarId);
+        const ws = ss.getSheetByName("【作業中】外勤");
+        const sheetInfo = ws
+          .getRange(2, 1, ws.getLastRow() - 1, ws.getLastColumn())
+          .getValues();
+        const schedules = [];
+        let error = 0;
+        sheetInfo.forEach(function (row, index) {
+          const title = row[2] + " " + row[0] + row[1];
+          const startTime = new Date(row[3]);
+          startTime.setHours(row[4]);
+          startTime.setMinutes(row[5]);
+          const endTime = new Date(row[6]);
+          endTime.setHours(row[7]);
+          endTime.setMinutes(row[8]);
+          const timeErrorExists = startTime.valueOf() >= endTime.valueOf();
+          const blankExists =
+            row
+              .map(function (value) {
+                return value === "";
+              })
+              .reduce(reduceSum) > 0;
+          if (timeErrorExists || blankExists) {
+            ws.getRange(2 + index, 10).setValue(
+              "この行の記載に空欄または誤りがあります(あとで消してください)"
+            );
+            error += 1;
+          }
+          schedules.push({
+            title: title,
+            startTime: startTime,
+            endTime: endTime,
+          });
+        });
+        if (error > 0) {
+          return "予定の記載に空欄または誤りがあります";
+        } else {
+          schedules.forEach(function (schedule) {
+            calendar.createEvent(
+              schedule.title,
+              schedule.startTime,
+              schedule.endTime
+            );
+            Utilities.sleep(500);
+          });
+          const today = new Date();
+          ws.setName(
+            today.toLocaleDateString() + " " + today.toLocaleTimeString()
+          );
+          const protection = ws.protect();
+          protection.setWarningOnly(true);
+          return "予定をカレンダーに登録しました";
+        }
+      } catch (e) {
+        Logger.log(e);
+        return "予定をカレンダーに登録できませんでした";
+      }
     }
   }
 }
